12e71807b56a4658cdcceee6ee3db3e8383e5427
NobodyXu <nobodyxu@lolol.me>
2019-09-14 12:20:07 +0000

    Simplify basic_pkgs/install.mk by supplying commands for all targets by pattern

--- basic_pkgs/install.mk
+++ basic_pkgs/install.mk
@@ -1,18 +1,6 @@
 All: ostree golang conmon runc configurations
 
-ostree:
-	./build_packages.sh $@
-
-golang:
-	./build_packages.sh $@
-
-conmon:
-	./build_packages.sh $@
-
-runc:
-	./build_packages.sh $@
-
-configurations:
+%:
 	./build_packages.sh $@
 
 PHONY: All ostree golang conmon runc configurations
